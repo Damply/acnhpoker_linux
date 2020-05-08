43a671e6459502ed1671d23d16112ad50967b761
Damply <31376489+Damply@users.noreply.github.com>
2020-05-07 20:47:18 -0400

    Add files via upload
    added options for inv slots up to 20, code could be cleaner. Will try to find a better method.

--- main.py
+++ main.py
@@ -7,14 +7,14 @@ import string
 invOffset = '0xAC4723D0'
 countOffset = '0xAC4723D4'
 
-def clear(): 
-    if name == 'nt': 
+def clear():
+    if name == 'nt':
         time.sleep(2)
-        _ = system('cls')  
-        spawnItem() 
-    else: 
+        _ = system('cls')
+        spawnItem()
+    else:
         time.sleep(2)
-        _ = system('clear') 
+        _ = system('clear')
         spawnItem()
 
 def is_hex(s):
@@ -23,7 +23,7 @@ def is_hex(s):
 
 def formatID(x):
     IDStr = str(x)
-    
+
     if( len(IDStr) > 4):
         print("input too large.")
         clear()
@@ -43,7 +43,7 @@ def sendCommand(s, content):
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #change the connection info here.
-s.connect(("192.168.254.85", 6000))
+s.connect(("192.168.1.51", 6000))
 
 
 
@@ -52,16 +52,61 @@ s.connect(("192.168.254.85", 6000))
 def spawnItem():
     user_input = input("Enter ItemID: ")
     if(is_hex(user_input)):
+
+        invslot = input("Which Slot?: ")
+
+        # i know there is a better way but its late and im tired.
+        if (invslot) == "1":
+            invOffset = '0xAC4723D0'
+        elif (invslot) == "2":
+            invOffset = '0xAC4723D8'
+        elif (invslot) == "3":
+            invOffset = '0xAC4723E0'
+        elif (invslot) == "4":
+            invOffset = '0xAC4723E8'
+        elif (invslot) == "5":
+            invOffset = '0xAC4723F0'
+        elif (invslot) == "6":
+            invOffset = '0xAC4723F8'
+        elif (invslot) == "7":
+            invOffset = '0xAC472400'
+        elif (invslot) == "8":
+            invOffset = '0xAC472408'
+        elif (invslot) == "9":
+            invOffset = '0xAC472410'
+        elif (invslot) == "10":
+            invOffset = '0xAC472418'
+        elif (invslot) == "11":
+            invOffset = '0xAC472420'
+        elif (invslot) == "12":
+            invOffset = '0xAC472428'
+        elif (invslot) == "13":
+            invOffset = '0xAC472430'
+        elif (invslot) == "14":
+            invOffset = '0xAC472438'
+        elif (invslot) == "15":
+            invOffset = '0xAC472440'
+        elif (invslot) == "16":
+            invOffset = '0xAC472448'
+        elif (invslot) == "17":
+            invOffset = '0xAC472450'
+        elif (invslot) == "18":
+            invOffset = '0xAC472458'
+        elif (invslot) == "19":
+            invOffset = '0xAC472460'
+        elif (invslot) == "20":
+            invOffset = '0xAC472468'
+
         itemString = "0x" + formatID(user_input)
-        
+
         pokeString = f"poke {invOffset} {itemString}"
 
-        sendCommand(s, pokeString.format(invOffset, itemString)) 
-        
+        sendCommand(s, pokeString.format(invOffset, itemString))
+
         itemCount = input("How Many?: ")
 
         if(is_hex(itemCount)):
-            sendCommand(s, f"poke {countOffset} {hex(int(itemCount) - 1)}") 
+            sendCommand(s, f"poke {countOffset} {hex(int(itemCount) - 1)}")
             print("Item(s) sent!")
             clear()
         else:
